018087b14378236893699b10b7ebd2086d64baa3
IIpho3nix <79083095+IIpho3nix@users.noreply.github.com>
2025-05-07 23:31:25 +0300

    prepare for compatibilty with other mod

--- src/main/java/me/iipho3nix/iicapemod/mixin/AbstractClientPlayerEntityMixin.java
+++ src/main/java/me/iipho3nix/iicapemod/mixin/AbstractClientPlayerEntityMixin.java
@@ -3,8 +3,9 @@ package me.iipho3nix.iicapemod.mixin;
 import me.iipho3nix.iicapemod.CapesMod;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.AbstractClientPlayerEntity;
-import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.client.util.SkinTextures;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -16,7 +17,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public class AbstractClientPlayerEntityMixin {
     @Inject(at = @At("RETURN"), method = "getSkinTextures", cancellable = true)
     private void getSkinTextures(CallbackInfoReturnable<SkinTextures> cir) {
-        if (((AbstractClientPlayerEntity) (Object) this) instanceof ClientPlayerEntity && CapesMod.capeCacheIdentifier != null) {
+        if (((AbstractClientPlayerEntity) (Object) this).getUuid().equals(MinecraftClient.getInstance().player.getUuid()) && CapesMod.capeCacheIdentifier != null && !FabricLoader.getInstance().isModLoaded("skinshuffle")) {
             SkinTextures textures = cir.getReturnValue();
             cir.setReturnValue(new SkinTextures(
                     textures.texture(),
